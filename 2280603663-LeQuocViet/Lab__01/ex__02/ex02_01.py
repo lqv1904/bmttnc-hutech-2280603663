9603c4306bd94b89c24f94760829874035a05738
lqv_- <lamtuanhau123@gmail.com>
2025-03-03 10:54:31 +0700

    Add files via upload

---
+++ 2280603663-LeQuocViet/Lab__01/ex__02/ex02_01.py
@@ -0,0 +1,6 @@
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+truong = input("Nhập trường của bạn đang học: ")
+print("Chào ní", ten)
+print("Bạn", tuoi,"tuổi")
+print("Bạn đang học",truong,"nè")
