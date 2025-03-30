61327ad1d4ac5644ec7b2563d6acef89b018a5b9
lqv_- <lamtuanhau123@gmail.com>
2025-03-30 17:01:51 +0700

    Delete bmttnc-hutech-2280603663-lab-03 directory

--- bmttnc-hutech-2280603663-lab-03/2280603663-LeQuocViet/Lab__01/ex__01/Hello.py
+++
@@ -1,5 +0,0 @@
-print("Xin chào mấy ní")
-print("Tui tên là Đạt")
-print("Tui 21 tuổi")
-print("Mã số sinh viên của tui là 2280619006")
-print("Lớp 22DTHA1")
--- bmttnc-hutech-2280603663-lab-03/2280603663-LeQuocViet/Lab__01/ex__02/ex02_01.py
+++
@@ -1,6 +0,0 @@
-ten = input("Nhập tên của bạn: ")
-tuoi = input("Nhập tuổi của bạn: ")
-truong = input("Nhập trường của bạn đang học: ")
-print("Chào ní", ten)
-print("Bạn", tuoi,"tuổi")
-print("Bạn đang học",truong,"nè")
